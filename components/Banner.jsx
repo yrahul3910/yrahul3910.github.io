429deed6df62c4b055532e2f3e6b0ca74b3d03d3
Rahul Yedida <yrahul3910@gmail.com>
2018-11-12 19:12:26 +0530

    Added basic information

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,7 +4,14 @@ export default class Banner extends React.Component {
     render() {
         return (
             <div className="nav">
-                Hi
+                <div className="profile-wrapper">
+                    <img src="../src/profile.png" />
+                </div>
+                <div className="profile-details">
+                    <h1>Rahul Yedida</h1>
+                    <h2>Undergraduate Student</h2>
+                    <h2>PES University, Electronic City</h2>
+                </div>
             </div>
         );
     }
