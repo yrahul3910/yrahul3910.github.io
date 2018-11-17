172df2f241f89d1020926172af7062e4339a5549
Rahul Yedida <yrahul3910@gmail.com>
2018-11-17 16:29:50 +0530

    Added introductory section

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -8,13 +8,19 @@ export default class Banner extends React.Component {
                     <img src="https://i.imgur.com/H8OmujV.png" />
                 </div>
                 <div className="profile-details">
-                    <h1>Rahul Yedida</h1>
+                    <h1>RAHUL YEDIDA</h1>
                     <h2>Undergraduate Student</h2>
-                    <h2>PES University, Electronic City</h2>
+                    <h2>
+                        <a href="https://pesitsouth.pes.edu">
+                            PES University, Electronic City
+                        </a>
+                    </h2>
+                    <br/><h2><em>(formerly PES Institute of Technology, South Campus)</em></h2>
                 </div>
                 <ul className="nav-menu-sticky">
                     <li>Research</li>
                     <li>Projects</li>
+                    <li><a href="https://1drv.ms/b/s!AiFT_8UzfVHdskGZUeaqLNACzkcR">CV</a></li>
                     <li>Contact</li>
                 </ul>
             </div>
--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import moment from "moment";
 import Banner from "./Banner.jsx";
 
 export default class MainPage extends React.Component {
@@ -8,6 +8,30 @@ export default class MainPage extends React.Component {
     }
 
     render() {
-        return <Banner />
+        return (
+            <div>
+                <Banner />
+                <main>
+                    <section>
+                        <h1>Hello!</h1>
+                        <p>
+                            I am Rahul Yedida, and I was born on October 27, 1997; alas, my surname initial is Y, which has led to a lot of "Why, Rahul?" jokes (hence the URL). I was born in Raipur, India. I grew up in Mississauga, Canada, and moved to Bangalore, India in 2006 to continue my middle schooling.
+                        </p>
+                        <p>
+                            Currently, I am a senior undergraduate student at PES University, Electronic City. This is my personal website, and any opinions presented here are my own and not those of my peers, professors, or of my institution.
+                        </p>
+                        <p>
+                            This website is simply for informative purposes, and you may safely assume that anything here is recent, though what time range defines "recent" is subjective, so in {moment().diff([1997, 10, 27], 'years')} years, one year may also be "recent". I will, however, try to ensure that this is not the case.
+                        </p>
+                        <p>
+                            This website section, titled "Hello!" is for introductory purposes. I am a strong advocate of privacy, and I support Mozilla's work in this frontier; indeed, it is difficult to spread awareness and fight for the citizens of the Internet. For this reason, I do not use Facebook, Messenger, or WhatsApp. I do, however, use Telegram, Skype, and email. Because of the convenience of its ecosystem, I use Outlook.com. I currently, quite happily, use Fedora. Its main selling point to me is its quick updates and <code>dnf</code>'s history feature.
+                        </p>
+                        <p>
+                            I love learning about how the world works, especially from a sociological perspective. Peter Berger's "Invitation to Sociology" and Emile Durkheim's "Suicide" have been revealing indeed. I also enjoy development, and right from high school, I have worked on developing desktop software and web applications. It is liberating to not need to rely on WYSIWYG editors and the like, and to be able to fully define what and how you want things to work, and to be able to write software to suit your own needs. Finally, I enjoy studying applied math and the working behind algorithms, as my peers will tell you with a sigh.
+                        </p>
+                    </section>                    
+                </main>
+            </div>
+        );
     }
 }
