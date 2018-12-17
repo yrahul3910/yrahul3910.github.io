5eb1958530324f7a5b357637d967f9105813f09c
Rahul Yedida <yrahul3910@gmail.com>
2018-12-17 16:51:35 +0530

    Finished complete 404 routing implementation

--- components/NotFound.jsx
+++ components/NotFound.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class NotFound extends React.Component {
+    componentDidMount() {
+        setTimeout(() => {
+            window.location.replace("/whyrahul");
+        }, 5000);
+    }
+
     render() {
         return (
             <main>
-                <h1>404 Not Found!</h1>
-                <h1>
-                    <Link style={{textDecoration: "underline", color: "blue"}} to="/whyrahul">
-                        WHY, RAHUL??
-                    </Link>
-                </h1>
-                <h2 style={{textAlign: "center"}}>Or, try the <em><Link style={{textDecoration: "underline", color: "blue"}} to="/whyrahul/tldr">Old Fashioned</Link></em> version.</h2>
+                <h1>404</h1>
+                <p style={{textAlign: "center"}}>You will be redirected to the main page in 5 seconds.</p>
+                <p style={{textAlign: "center"}}>Or, <Link to="/whyrahul">click here</Link> to redirect now.</p>
             </main>
         );
     }
--- dist.sh
+++ dist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf dist/
 mkdir dist/
-cp src/404.html dist/
+cp src/404.html src/ghspa.js dist/
 npm run deploy
