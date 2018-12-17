79a22ea7e94f579158ce448383c38f5251da1bf0
Rahul Yedida <yrahul3910@gmail.com>
2018-12-17 15:45:51 +0530

    Cut out more text

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -39,9 +39,6 @@ export default class MainPage extends React.Component {
                             I am Rahul Yedida, and I was born on October 27, 1997; alas, my surname initial is Y, which has led to a lot of "Why, Rahul?" jokes (hence the URL). I was born in Raipur, India. I grew up in Mississauga, Canada, and moved to Bangalore, India in 2006 to continue my middle schooling.
                         </p>
                         <p>
-                            Currently, I am a senior undergraduate student at PES University, Electronic City. This is my personal website, and any opinions presented here are my own and not those of my peers, professors, or of my institution.
-                        </p>
-                        <p>
                             I love learning about how the world works, especially from a sociological perspective. Peter Berger's "Invitation to Sociology" and Emile Durkheim's "Suicide" have been revealing indeed. I also enjoy development, and right from high school, I have worked on developing desktop software and web applications. It is liberating to not need to rely on WYSIWYG editors and the like, and to be able to fully define what and how you want things to work, and to be able to write software to suit your own needs. Finally, I enjoy studying applied math and the working behind algorithms, as my peers will tell you with a sigh.
                         </p>
                     </section> 
@@ -70,10 +67,10 @@ export default class MainPage extends React.Component {
                     <section id="projects" className="green-background">
                         <h1>Projects</h1>
                         <p>
-                            I consider myself fluent in the MERN stack, Python/Flask, and to some extent, C# and VB.NET. I am happy to do full-stack work, though I have some preference for back end development (we all know it is the easier part!) I work well in Agile teams with people who can pick up work independently and discuss any trouble they have with their share of the work.
+                            I consider myself fluent in the MERN stack, Python/Flask, and to some extent, C# and VB.NET. I work well in Agile teams with people who can pick up work independently and discuss any trouble they have with their share of the work.
                         </p>
                         <p>
-                            The below is only a small sample of my project work. For a more comprehensive list of my notable projects, please view my CV. My <a href="https://www.github.com/yrahul3910">GitHub profile</a> unfortunately has a few projects that I keep telling myself that I will get back to!
+                            Below is a sample of my projects. For a more comprehensive list of my notable projects, please view my CV. My <a href="https://www.github.com/yrahul3910">GitHub profile</a> unfortunately has a few projects that I keep telling myself that I will get back to!
                         </p>
                         <CardGrid cards={projects} />
                     </section>
