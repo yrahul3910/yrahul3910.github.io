0f1cbadae20ab8bc19029cb1a4b0959e38f88d64
Rahul Yedida <yrahul3910@gmail.com>
2019-03-02 13:31:17 +0530

    Implemented expandable cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.click = this.click.bind(this);
+    }
+
+    click(e) {
+        // Toggle div visibility.
+        e.currentTarget.parentElement.classList.toggle("active");
+        e.currentTarget.nextSibling.classList.toggle("visible");
+    }
+
     render() {
         return <div className="card">
-            <h2 className="card-title">{this.props.title}</h2>
-            <div>{this.props.info}</div>
+            <h2 className="card-title" id={this.props.title.replace(' ', '')} onClick={this.click}>
+                {this.props.title}
+            </h2>
+            <div id={"div" + this.props.title.replace(' ', '')}>{this.props.info}</div>
         </div>
     }
 }
