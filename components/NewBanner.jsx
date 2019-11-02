0761ef54622531b0933245d6e89c22c9d8419d76
Rahul Yedida <yrahul3910@gmail.com>
2019-11-01 23:46:58 -0400

    Updated banner

--- components/NewBanner.jsx
+++ components/NewBanner.jsx
@@ -8,22 +8,30 @@ export default class NewBanner extends React.Component {
                     
                 </div>
                 <div className="nav">
-                    <div className="profile-details">
-                        <h1>RAHUL YEDIDA</h1>
+                    <div id="banner-profile" className="profile-details">
+                        <h1>Rahul Yedida</h1>
                         <h2>Ph.D. Student</h2>
                         <h2>Department of Computer Science</h2>
                         <h2 style={{ marginBottom: "10vh" }}>
                             <a href="https://csc.ncsu.edu">
-                                NC State University
+                                North Carolina State University
                             </a>
                         </h2>
                         <h1>About Me</h1>
                         <ul>
-                            <li><b>Born:</b> October 27, 1997</li>
                             <li><b>Experience:</b> Machine learning, web development, .NET</li>
-                            <li><b>Hobbies:</b> Programming, chess, reading</li>
-                            <li><b>Interests:</b> Sociology, math, behavioral neuroscience</li>
+                            <li><b>Open to:</b> Research scientist / data scientist / software engineer intern </li>
                         </ul>
+
+                        <div style={{ fontSize: 18 }}>
+                            I convert coffee to 
+                            <div class="rotating">
+                                <span>code.</span>
+                                <span>research.</span>
+                                <span>less coffee.</span>
+                            </div>
+                            <p>So what can I help you with?</p>
+                        </div>
                     </div>
                 </div>
             </div>
